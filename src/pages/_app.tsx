408a1fd8ecdfd895431afd6b377546b60c170f8c
kike <luisenriquep110101@gmail.com>
2025-03-31 01:31:17 -0600

    falta costos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,10 @@ import theme from "../Theme/theme";
 import { ApiProvider } from "@/context/ApiContext";
 export default function App({ Component, pageProps }: AppProps) {
 
-
+ // NEXT_PUBLIC_API_URL_MATERIALS=https://backnode-production.up.railway.app/api/materials
+  //NEXT_PUBLIC_API_URL_DERIVATIVES=https://backnode-production.up.railway.app/api/derivatives
+  //NEXT_PUBLIC_API_URL_RESISTANCES=https://backnode-production.up.railway.app/api/resistances
+  //NEXT_PUBLIC_API_URL_CATEGORIES=https://backnode-production.up.railway.app/api/resistancescategories
 return (
   <ThemeProvider theme={theme}>
     {/* Proveedor de tema para Material-UI */}
